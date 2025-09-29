e2d517528adb4e10014c3dbcad1ff4f96e88aaa0
newbie421 <newbiesooc@gmail.com>
2025-09-29 14:58:22 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,32 +13,25 @@ export KBUILD_BUILD_USER="github-actions"
 export CLANG_DIR="${HOME}/toolchains/clang-r563880b"
 export PATH="${CLANG_DIR}/bin:${PATH}"
 
-# cross compile
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export CC=clang
-export LD=ld.lld
+# Clang + LLVM
+export LLVM=1
+export LLVM_IAS=1
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
 # Create necessary directories
 mkdir -p "${KERNEL_ROOT}/out" "${KERNEL_ROOT}/build"
 
 build_kernel(){
     # defconfig
-    make -C ${KERNEL_ROOT} O=${KERNEL_ROOT}/out \
-        ARCH=arm64 LLVM=1 LLVM_IAS=1 \
-        CROSS_COMPILE=${CROSS_COMPILE} \
-        CROSS_COMPILE_ARM32=${CROSS_COMPILE_ARM32} \
-        CC=${CC} -j$(nproc) gki_defconfig
+    make -C ${KERNEL_ROOT} O=${KERNEL_ROOT}/out ARCH=arm64 gki_defconfig
 
-    # build Image
-    make -C ${KERNEL_ROOT} O=${KERNEL_ROOT}/out \
-        ARCH=arm64 LLVM=1 LLVM_IAS=1 \
-        CROSS_COMPILE=${CROSS_COMPILE} \
-        CROSS_COMPILE_ARM32=${CROSS_COMPILE_ARM32} \
-        CC=${CC} -j$(nproc) Image || exit 1
+    # build kernel
+    make -C ${KERNEL_ROOT} O=${KERNEL_ROOT}/out ARCH=arm64 \
+        -j$(nproc) Image.gz-dtb || exit 1
 
     # copy hasil
-    cp "${KERNEL_ROOT}/out/arch/arm64/boot/Image" "${KERNEL_ROOT}/build"
+    cp "${KERNEL_ROOT}/out/arch/arm64/boot/Image.gz-dtb" "${KERNEL_ROOT}/build"
 
     echo -e "\n[INFO]: BUILD FINISHED..!\n"
 }
